9f5b0f2ecbfa83c3b096bc95c5c09d8a2177bfe6
Sylvain Le Breton <40246048+SylvainLeBreton@users.noreply.github.com>
2021-04-16 14:35:25 +0200

    added webdeploy

--- Windows/IIS-basics-v1.ps1
+++ Windows/IIS-basics-v1.ps1
@@ -1,3 +1,4 @@
+#IIS
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
@@ -10,6 +11,23 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools  # to allow Import-Module WebAdministration
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
-
 # Configure IIS with powershell
 Import-Module WebAdministration 
+
+#Installers
+
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install webpicmd -y
+
+#Web Deploy
+
+choco install webdeploy -y
+WebpiCmd.exe /Install /AcceptEULA /SuppressReboot /Products:WDeploy_2_1
+#& "WebpiCmd-x64.exe" /Install /AcceptEULA /SuppressReboot /Products:WDeploy36NoSMO
+WebpiCmd.exe /Install /AcceptEULA /SuppressReboot /Products:WDeploy36PS
+# Should be uninstalled and reinstalled for some reasons...
+# https://stackoverflow.com/a/40722931/7361736
+choco uninstall webdeploy -y
+choco install webdeploy -y
+New-NetFirewallRule -DisplayName 'WebDeploy 8172' -Profile 'Private' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8172
